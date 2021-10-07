111395b44f98f218a050d0c5e56ff167a8137e89
Udai1931 <udaigupta19311@gmail.com>
2021-10-07 12:01:35 +0530

    full course

---
+++ ip/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Parent from './Parent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hooks</h1>
+      <Parent/>
+    </div>
+  );
+}
+
+export default App;
